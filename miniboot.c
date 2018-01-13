833d946952eb46afea5f0571c77f7204c5153bdf
Mihai Galos <mihaigalos@gmail.com>
2018-01-13 22:10:50 +0100

    Jump to application after bootloader finished

--- miniboot.c
+++ miniboot.c
@@ -15,9 +15,11 @@ static inline uint16_t getDataLength(uint8_t i2c_address) {
   return getWordFromSource(i2c_address, 32);  
 }
 
-void writeToFlash(uint16_t address, uint8_t *data) {
+static inline void writeToFlash(uint16_t address, uint8_t *data, uint16_t& application_start) {
+  
   if (0 == address) {
-    
+    application_start = static_cast<uint8_t>(data[RESET_VECTOR_ARGUMENT_ADDRESS]);
+    application_start |= static_cast<uint8_t>((static_cast<uint16_t>(data[RESET_VECTOR_ARGUMENT_ADDRESS+1]))<<8);
     data[RESET_VECTOR] = static_cast<uint8_t>(jmp_instruction);
     data[RESET_VECTOR+1] = static_cast<uint8_t>(jmp_instruction >> 8);
     data[RESET_VECTOR_ARGUMENT_ADDRESS] = static_cast<uint8_t>(static_cast<uint16_t>(BOOTLOADER_START_ADDRESS) / 2);
@@ -32,7 +34,7 @@ void delay(volatile uint16_t count) {
     ;
 }
 
-static inline void writeFlashFromI2C(uint8_t i2c_address) {
+static inline void writeFlashFromI2C(uint8_t i2c_address, uint16_t& application_start) {
   uint16_t start_address = getDataStartAddressInSource(i2c_address);
   uint16_t length = getDataLength(i2c_address);
   uint8_t buf[SPM_PAGESIZE];
@@ -43,7 +45,7 @@ static inline void writeFlashFromI2C(uint8_t i2c_address) {
   for (uint16_t pos = 0; pos < word_count; pos += 2) {
 
     if (pos > 0 && (0 == (pos % SPM_PAGESIZE))) {
-      writeToFlash(writes * SPM_PAGESIZE, &buf[0]);
+      writeToFlash(writes * SPM_PAGESIZE, &buf[0], application_start);
       LED_TOGGLE();
       ++writes;
     }
@@ -53,11 +55,18 @@ static inline void writeFlashFromI2C(uint8_t i2c_address) {
   }
 }
 
+static void leaveBootloader(uint16_t& application_start) __attribute__((__noreturn__));
+static inline void leaveBootloader(uint16_t& application_start) {
+  //hold my beer and watch this!
+  reinterpret_cast<void(*)(void)>(application_start)();
+  while(1);
+}
 int main() {
   init();
   eraseApplication();
-  writeFlashFromI2C(source_i2c_address_for_program);
-  while(1);
+  uint16_t application_start = 0;
+  writeFlashFromI2C(source_i2c_address_for_program, application_start);
+  leaveBootloader(application_start);
   
   return 0;
 }

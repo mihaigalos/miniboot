b7534959d9dd9e06dfb3585133478b7d44242824
Mihai Galos <mihaigalos@gmail.com>
2018-01-13 21:00:06 +0100

    Working jump to bootloader

--- miniboot.c
+++ miniboot.c
@@ -8,22 +8,20 @@
 #include "init.h"
 
 static inline uint16_t getDataStartAddressInSource(uint8_t i2c_address) {
-  return 0;
+  return 34;
 }
 
 static inline uint16_t getDataLength(uint8_t i2c_address) {
-  // multiple of SPM_PAGESIZE (128)
-  return 128 * 30;
+  return getWordFromSource(i2c_address, 32);  
 }
 
-void adjustAddresses(uint16_t address, uint8_t *data) {
+void writeToFlash(uint16_t address, uint8_t *data) {
   if (0 == address) {
-    data[RESET_VECTOR] = static_cast<uint8_t>(jmp_instruction >> 8);
+    
     data[RESET_VECTOR] = static_cast<uint8_t>(jmp_instruction);
-    data[RESET_VECTOR_ARGUMENT_ADDRESS] =
-        static_cast<uint8_t>(BOOTLOADER_START_ADDRESS / 2 >> 8);
-    data[RESET_VECTOR_ARGUMENT_ADDRESS] =
-        static_cast<uint8_t>(BOOTLOADER_START_ADDRESS / 2);
+    data[RESET_VECTOR+1] = static_cast<uint8_t>(jmp_instruction >> 8);
+    data[RESET_VECTOR_ARGUMENT_ADDRESS] = static_cast<uint8_t>(static_cast<uint16_t>(BOOTLOADER_START_ADDRESS) / 2);
+    data[RESET_VECTOR_ARGUMENT_ADDRESS+1] = static_cast<uint8_t>((static_cast<uint16_t>(BOOTLOADER_START_ADDRESS) / 2) >> 8);
   }
   writeToPageBuffer(address, data);
   writePageBufferToFlash(address);
@@ -42,23 +40,24 @@ static inline void writeFlashFromI2C(uint8_t i2c_address) {
 
   uint16_t word_count = length / 2 + length % 2;
 
-  for (uint16_t pos = start_address; pos < length / 2; pos += 2) {
+  for (uint16_t pos = 0; pos < word_count; pos += 2) {
 
     if (pos > 0 && (0 == (pos % SPM_PAGESIZE))) {
-      adjustAddresses(writes * SPM_PAGESIZE, &buf[0]);
+      writeToFlash(writes * SPM_PAGESIZE, &buf[0]);
       LED_TOGGLE();
       ++writes;
     }
-    uint16_t data = getWordFromSource(i2c_address, pos);
+    uint16_t data = getWordFromSource(i2c_address, pos+start_address);
     buf[pos % SPM_PAGESIZE] = static_cast<uint8_t>(data); //
-    buf[(pos + 1) % SPM_PAGESIZE] = static_cast<uint8_t>(data >> 8);
-    delay(1000);
+    buf[(pos + 1) % SPM_PAGESIZE] = static_cast<uint8_t>(data >> 8);    
   }
 }
 
 int main() {
   init();
-  // eraseApplication();
+  eraseApplication();
   writeFlashFromI2C(source_i2c_address_for_program);
+  while(1);
+  
   return 0;
 }

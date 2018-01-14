3de8069052075fbfae96de670335a516a47f6cce
Mihai Galos <mihaigalos@gmail.com>
2018-01-14 22:48:41 +0100

    E2PROM app writes to CAT24M01 on serial uart data

--- e2prom/e2prom.ino
+++ e2prom/e2prom.ino
@@ -30,10 +30,10 @@ void setup() {
     //e.dump(43);
     //uint8_t buffer[] = {"AAA!"};
     //e.writePage(10, &buffer[0], 5);
-     e.dump();
-      //ihex.parse_intel_hex_from_flash();
+    //e.dump();
+    //ihex.parse_intel_hex_from_flash();
 }
 
 void loop() {
-  //ihex.write_to_eeprom_i2c(0x50,2);
+  ihex.write_to_eeprom_i2c(0x50,2);
 }

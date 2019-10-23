2de88efbcb2c5ef67f10d7d9471a26f5cfc94872
Mihai Galos <mihaigalos@gmail.com>
2019-10-23 20:45:59 +0200

    Remove request type, sed quote strip

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -5,10 +5,9 @@ make_log=$1
 
 miniboot_elf=$(cat $make_log | grep -A 8 "miniboot.elf  :" | head -n 8 | sed -e ':a;N;$!ba;s/\n/\\n/g')
 markdown_ify="\`\`\`bash\n${miniboot_elf}\n\`\`\`"
-push_message="[CI Auto Message]\n${markdown_ify}" | sed -e "s/'//"
+push_message="[CI Auto Message]\n${markdown_ify}"
 
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -H "Content-Type: text; charset=utf-8" \
     -d "{"body": "${push_message}"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"

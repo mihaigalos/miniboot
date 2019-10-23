237fb1abf06f83f48516de66bc16a95b4297e196
Mihai Galos <mihaigalos@gmail.com>
2019-10-23 18:13:26 +0200

    Post miniboot summary back to PR

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+miniboot_elf=$(curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" | grep -A 8 "miniboot.elf  :")
+
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Hello world\"}" \
+    -d "{"body": "$miniboot_elf"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"

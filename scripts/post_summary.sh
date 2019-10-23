83d11bd05f71623621aa4ef51c8b53056d9ee473
Mihai Galos <mihaigalos@gmail.com>
2019-10-23 18:25:36 +0200

    Only take first 8 lines from match of log

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x #echo on
 
-miniboot_elf=$(curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" | grep -A 8 "miniboot.elf  :")
+miniboot_elf=$(curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" | grep -A 8 "miniboot.elf  :" | head -n 8)
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{"body": "$miniboot_elf"}" \

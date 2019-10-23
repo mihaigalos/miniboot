709c87bbc291c1ded7638cb1aa5c1d171c4af5be
Mihai Galos <mihaigalos@gmail.com>
2019-10-23 19:41:32 +0200

    Add request type to curl

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -6,5 +6,6 @@ make_log=$1
 miniboot_elf=$(cat $make_log | grep -A 8 "miniboot.elf  :" | head -n 8)
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -H "Content-Type: text; charset=utf-8" \
     -d "{"body": "$miniboot_elf"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"

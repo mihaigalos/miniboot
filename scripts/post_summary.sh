018b36a947bc65f304be7f41f8af9c52056b52e3
Mihai Galos <mihaigalos@gmail.com>
2019-10-23 20:24:17 +0200

    Reconcile conflicts

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -4,8 +4,8 @@ set -x #echo on
 make_log=$1
 
 miniboot_elf=$(cat $make_log | grep -A 8 "miniboot.elf  :" | head -n 8 | sed -e ':a;N;$!ba;s/\n//g')
-add_bash_frame="```bash\n${miniboot_elf}\n```"
-push_message="[CI Auto Message]\n${add_bash_frame}"
+markdown_ify="\`\`\`bash\n${miniboot_elf}\n\`\`\`"
+push_message="[CI Auto Message]\n${markdown_ify}"
 
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \

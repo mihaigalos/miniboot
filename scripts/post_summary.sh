579425b10bd467950763d2bf54acb3c118bdd0ff
Mihai Galos <mihaigalos@gmail.com>
2019-10-24 21:50:18 +0200

    Escape double quotes

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -6,5 +6,5 @@ make_log=$1
 push_message="[CI Auto Message]\n\`\`\`bash\n$(cat $make_log | grep -A 8 "miniboot.elf  :" | head -n 8 | sed -e ':a;N;$!ba;s/\n/\\n/g')\n\`\`\`"
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{"body": "${push_message}"}" \
+    -d "{\"body\": \"${push_message}\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"

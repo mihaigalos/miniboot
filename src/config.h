c412431358b2d785964a1688903197986b589e97
Tony Sesek <leorijn223@gmail.com>
2020-01-14 17:54:24 -0800

    Move offsets to sequential order / Make CRC fit match wording

--- src/config.h
+++ src/config.h
@@ -11,7 +11,7 @@ static constexpr uint32_t eeprom_not_programmed =
 static constexpr uint16_t eeprom_address_offset = 0x0000;
 
 static constexpr uint16_t timestamp_application_byte_offset = 20 + eeprom_address_offset;
+static constexpr uint16_t application_crc_expected_offset = 28 + eeprom_address_offset;
 static constexpr uint16_t application_length_byte_offset = 32 + eeprom_address_offset;
 static constexpr uint16_t application_byte_offset = 34 + eeprom_address_offset;
 static constexpr uint16_t application_start_address_byte_offset = 36 + eeprom_address_offset;
-static constexpr uint16_t application_crc_expected_index = 28 + eeprom_address_offset;

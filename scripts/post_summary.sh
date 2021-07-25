1b92af5006fa7e56b8866390c75cbf92acdea3bf
Mihai Galos <mihaigalos@gmail.com>
2021-07-25 10:19:12 +0200

    Disable CI messages for non-PR changes

--- scripts/post_summary.sh
+++ scripts/post_summary.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-function fail {
+function my_exit {
     printf '%s\n' "$1" >&2 
     exit "${2-1}"  ## Return a code specified by $2 or 1 by default.
 }
@@ -10,13 +10,17 @@ function fail {
 build_log=$1
 POST_SUMMARY_TOKEN=$2
 
-[ ! -f "${build_log}" ] && fail "Build log file not passed as argument to script. Exiting."
-[ -z "${POST_SUMMARY_TOKEN}" ] && fail "POST_SUMMARY_TOKEN variable is not set. Exiting."
+[ ! -f "${build_log}" ] && my_exit "Build log file not passed as argument to script. Exiting."
+[ -z "${POST_SUMMARY_TOKEN}" ] && my_exit "POST_SUMMARY_TOKEN variable is not set. Exiting."
 
 header="CI Auto Message"
 body="$(cat $build_log | grep -A 5 "Program: " | head -n 5 | sed -e ':a;N;$!ba;s/\n/\\n/g')"
 footer=""
 PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+
+
+[[ $PR_NUMBER == "master" ]] && my_exit "Change is not a PR, no need to continue. Exiting." 0
+[[ $PR_NUMBER == "main" ]] && my_exit "Change is not a PR, no need to continue. Exiting." 0
 
 printf -v push_message '`%s`\\n---\\n```bash\\n%s\\n```\\n%s' "$header" "$body" "$footer"
 
